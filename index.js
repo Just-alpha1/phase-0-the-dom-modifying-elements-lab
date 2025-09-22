34d760a3587b49fce12e8565a5801a2a8b58998d
Collins Kiaritha <thuocollins020@gmail.com>
2025-09-22 15:31:06 +0300

    push

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the main element with id 'main'
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new h1 element and assign it to newHeader variable
+const newHeader = document.createElement('h1');
+
+// Give the h1 element an id of 'victory'
+newHeader.id = 'victory';
+
+// Set the innerHTML to contain "YOUR-NAME is the champion"
+newHeader.innerHTML = 'Collins is the champion';
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
